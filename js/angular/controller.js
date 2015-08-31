1fc801b9dadfeb2a42c63e73875da56871bba500
hmarikan <hmarikan@corp.untd.com>
2015-08-31 14:42:39 +0530

    better ui

--- js/angular/controller.js
+++ js/angular/controller.js
@@ -1,6 +1,9 @@
-myFirstApp.controller('mainController', function($scope, $route){
+myFirstApp.controller('mainController', function($scope, $route, $window){
 
+  $scope.openSite = function () {
+    $window.open('https://labwise.in', '_blank');
 
+  };
 
 });
 
@@ -8,14 +11,33 @@ myFirstApp.controller('registerController', function($scope, $route){
   $scope.chooseTrue = true;
   $scope.providerView = false;
   $scope.providerView2 = false;
-  $scope.UserView = false;
-  
+  $scope.userView = false;
+
 
   $scope.providerSelected = function () {
 
     $scope.providerView = true;
     $scope.chooseTrue = false;
   };
+
+  $scope.userSelected = function () {
+
+    $scope.providerView = false;
+    $scope.chooseTrue = false;
+    $scope.userView = true;
+  };
+
+  $scope.submitProvideFormView1 = function() {
+    $scope.providerView = false;
+    $scope.chooseTrue = false;
+    $scope.providerView2 = true;
+
+
+  };
+
+  $scope.saveProvider = function () {
+    alert('saving' + $scope.lab + $scope.pname + $scope.email)
+  }
 
 
 
